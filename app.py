78c087d5d552ab07912a120ddc0535808eb007cc
rishi <rishik8340@gmail.com>
2026-09-23 19:41:25 +0530

    hii

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello! Python App deployed using GitHub Actions and Argo CD."
+
+
+@app.route("/health")
+def health():
+    return "Application is healthy"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
